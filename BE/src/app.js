7ef25f8044edc224b7349ef2375dd299e917d198
Fajar Teguh Permana <fajarcode21@gmail.com>
2025-11-16 20:44:30 +0700

    initialize project and install initial dependencies

--- BE/src/app.js
+++ BE/src/app.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
+
+import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
+
+app.use(express.json());
+app.use(cors());
+
+app.use(errorHandler);
 
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
