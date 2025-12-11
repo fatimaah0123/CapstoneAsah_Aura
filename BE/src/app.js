80a19de18c0a0f2d6e7ece410b750da0e8873b84
Fajar Teguh Permana <fajarcode21@gmail.com>
2025-12-12 02:03:10 +0700

    add cors

--- BE/src/app.js
+++ BE/src/app.js
@@ -17,7 +17,14 @@ const swaggerDocument = JSON.parse(
 );
 
 app.use(express.json({ limit: '5mb' }));
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://capstone-asah2025-fe.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 
 app.use(Routes);
 
